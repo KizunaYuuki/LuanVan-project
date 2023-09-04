ad512a2766808c586a6d2a92361af00669bc2efa
KizunaYuuki <time0009000@gmail.com>
2023-09-04 16:13:53 +0700

    #3 Fix footer

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -1,7 +1,8 @@
 <template>
-    <footer class="relative border-t-[1.5px] border-[#0096fa]">
-        <div class="absolute inset-x-0 flex items-center justify-center -top-3">
-            <!-- <div class="relative flex justify-center h-10 w-[9.17rem]">
+    <footer class="bg-white relative border-t-[1.5px] border-[#0f172a1a]">
+        <div class="max-w-[1280px] mx-auto min-[1024px]:px-[32px]">
+            <div class="absolute inset-x-0 flex items-center justify-center -top-3">
+                <!-- <div class="relative flex justify-center h-10 w-[9.17rem]">
                 <div class="absolute w-full h-full inset-0 -top-[2px] backdrop-blur-sm u-bg-white rounded-full">
                     <p class="-m-1.5 p-1.5 text-[1rem] font-[700] tracking-[.125rem] text-[#0097f9] subpixel-antialiased">Moving Service</p>
                 </div>
@@ -14,12 +15,12 @@
                         fill="#00DC82"></path>
                 </svg>
             </div> -->
-        </div>
-        <div class="container pt-[1.5rem] pb-[16px] mx-auto">
-            <div class="sm:relative flex flex-col items-center gap-3 sm:flex-row sm:justify-between">
-                <div class="flex flex-col-reverse items-center gap-3 sm:flex-row sm:w-40 md:w-60">
-                    <!-- Dark mode -->
-                    <!-- <input type="checkbox" hidden="" readonly="" name="theme"
+            </div>
+            <div class="container pt-[1.5rem] pb-[16px] mx-auto">
+                <div class="sm:relative flex flex-col items-center gap-3 sm:flex-row sm:justify-between">
+                    <div class="flex flex-col-reverse items-center gap-3 sm:flex-row sm:w-40 md:w-60">
+                        <!-- Dark mode -->
+                        <!-- <input type="checkbox" hidden="" readonly="" name="theme"
                         style="position: fixed; height: 0px; padding: 0px; overflow: hidden; clip: rect(0px, 0px, 0px, 0px); white-space: nowrap; border-width: 0px; display: none;">
                     <button class="app-toggle theme-select z-10 order-1 sm:order-none" size="sm" id="headlessui-switch-3"
                         role="switch" type="button" tabindex="0" aria-checked="false" data-headlessui-state=""><span
@@ -41,85 +42,92 @@
                                     </path>
                                 </svg></span><span class="sr-only">Toggle theme</span></span>
                     </button> -->
-                    <span class="text-sm u-text-gray-500">© 2023 Moving Service</span>
-                </div>
-                <ul class="flex flex-wrap justify-center text-sm gap-x-6 gap-y-2"><!--[-->
-                    <li><a href="/support"
-                            class="font-medium u-text-gray-500 hover:u-text-gray-900 focus:u-text-gray-900">Vận chuyển</a>
-                    </li>
-                    <li><a href="/tracking"
-                            class="font-medium u-text-gray-500 hover:u-text-gray-900 focus:u-text-gray-900">Theo dõi</a>
-                    </li>
-                    <li><a href="/support" rel="noopener noreferrer"
-                            class="font-medium u-text-gray-500 hover:u-text-gray-900 focus:u-text-gray-900">Trợ giúp & Hỗ
-                            trợ</a>
-                    </li>
-                    <li><a href="/contact" rel="noopener noreferrer"
-                            class="font-medium u-text-gray-500 hover:u-text-gray-900 focus:u-text-gray-900">Liên hệ với
-                            chúng tôi</a>
-                    </li><!--]-->
-                </ul>
-                <ul class="flex items-center justify-end gap-1.5 sm:-mr-0 sm:gap-3 z-10 sm:w-40 md:w-60 drop-shadow">
-                    <li>
-                        <a href="https://twitter.com/" rel="noopener noreferrer" target="_blank"
-                            class="app-button pv-WjEgmP pc-MowtcP !p-0 u-text-gray-900 focus-visible:ring-2"
-                            title="Follow Nuxt on Twitter" data-v-d64ec27c="">
-                            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                                aria-hidden="true" role="img" class="icon icon-leading fill-current text-[#1d9bf0]" data-v-d64ec27c="" style=""
-                                width="1em" height="1em" viewBox="0 0 24 24" data-v-40670dad="">
-                                <path fill="currentColor"
-                                    d="M22 5.8a8.49 8.49 0 0 1-2.36.64a4.13 4.13 0 0 0 1.81-2.27a8.21 8.21 0 0 1-2.61 1a4.1 4.1 0 0 0-7 3.74a11.64 11.64 0 0 1-8.45-4.29a4.16 4.16 0 0 0-.55 2.07a4.09 4.09 0 0 0 1.82 3.41a4.05 4.05 0 0 1-1.86-.51v.05a4.1 4.1 0 0 0 3.3 4a3.93 3.93 0 0 1-1.1.17a4.9 4.9 0 0 1-.77-.07a4.11 4.11 0 0 0 3.83 2.84A8.22 8.22 0 0 1 3 18.34a7.93 7.93 0 0 1-1-.06a11.57 11.57 0 0 0 6.29 1.85A11.59 11.59 0 0 0 20 8.45v-.53a8.43 8.43 0 0 0 2-2.12Z">
-                                </path>
-                            </svg>
-                            <span data-v-d64ec27c="">
-                                <span class="sr-only">Follow Us on
-                                    Twitter
+                        <span class="text-sm u-text-gray-500">© 2023 Moving Service</span>
+                    </div>
+                    <ul class="flex flex-wrap justify-center text-sm gap-x-6 gap-y-2"><!--[-->
+                        <li><a href="/support"
+                                class="font-medium u-text-gray-500 hover:u-text-gray-900 focus:u-text-gray-900">Vận
+                                chuyển</a>
+                        </li>
+                        <li><a href="/tracking"
+                                class="font-medium u-text-gray-500 hover:u-text-gray-900 focus:u-text-gray-900">Theo dõi</a>
+                        </li>
+                        <li><a href="/support" rel="noopener noreferrer"
+                                class="font-medium u-text-gray-500 hover:u-text-gray-900 focus:u-text-gray-900">Trợ giúp &
+                                Hỗ
+                                trợ</a>
+                        </li>
+                        <li><a href="/contact" rel="noopener noreferrer"
+                                class="font-medium u-text-gray-500 hover:u-text-gray-900 focus:u-text-gray-900">Liên hệ với
+                                chúng tôi</a>
+                        </li><!--]-->
+                    </ul>
+                    <ul class="flex items-center justify-end gap-1.5 sm:-mr-0 sm:gap-3 z-10 sm:w-40 md:w-60 drop-shadow">
+                        <li>
+                            <a href="https://twitter.com/" rel="noopener noreferrer" target="_blank"
+                                class="app-button pv-WjEgmP pc-MowtcP !p-0 u-text-gray-900 focus-visible:ring-2"
+                                title="Follow Nuxt on Twitter" data-v-d64ec27c="">
+                                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                                    aria-hidden="true" role="img" class="icon icon-leading fill-current text-[#1d9bf0] hover:scale-[1.2] transition-all duration-[0.3s] ease-in-out delay-[0ms]"
+                                    data-v-d64ec27c="" style="" width="1em" height="1em" viewBox="0 0 24 24"
+                                    data-v-40670dad="">
+                                    <path fill="currentColor"
+                                        d="M22 5.8a8.49 8.49 0 0 1-2.36.64a4.13 4.13 0 0 0 1.81-2.27a8.21 8.21 0 0 1-2.61 1a4.1 4.1 0 0 0-7 3.74a11.64 11.64 0 0 1-8.45-4.29a4.16 4.16 0 0 0-.55 2.07a4.09 4.09 0 0 0 1.82 3.41a4.05 4.05 0 0 1-1.86-.51v.05a4.1 4.1 0 0 0 3.3 4a3.93 3.93 0 0 1-1.1.17a4.9 4.9 0 0 1-.77-.07a4.11 4.11 0 0 0 3.83 2.84A8.22 8.22 0 0 1 3 18.34a7.93 7.93 0 0 1-1-.06a11.57 11.57 0 0 0 6.29 1.85A11.59 11.59 0 0 0 20 8.45v-.53a8.43 8.43 0 0 0 2-2.12Z">
+                                    </path>
+                                </svg>
+                                <span data-v-d64ec27c="">
+                                    <span class="sr-only">Follow Us on
+                                        Twitter
+                                    </span>
                                 </span>
-                            </span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="tel:+84332006710" rel="noopener noreferrer" target="_blank"
-                            class="app-button pv-WjEgmP pc-lW04s6 !p-0 u-text-gray-900 focus-visible:ring-2" title="Call Us"
-                            data-v-d64ec27c="">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="fill-current text-[#4bd964]" id="phone"
-                                width="1em" height="1em" version="1.1" viewBox="0 0 29 29" xml:space="preserve">
-                                <path
-                                    d="M5.661 23.339c4.882 4.882 12.796 4.882 17.678 0 4.882-4.882 4.882-12.796 0-17.678s-12.796-4.882-17.678 0C.78 10.543.78 18.457 5.661 23.339zm5.545-16.613l1.86 1.86c.372.372.451.947.193 1.406l-.699 1.241a.781.781 0 0 0 .129.937l4.141 4.141a.781.781 0 0 0 .937.128l1.241-.699a1.173 1.173 0 0 1 1.406.193l1.861 1.861a1.175 1.175 0 0 1 0 1.66l-.881.881a3.135 3.135 0 0 1-3.786.493l-.084-.048a24.54 24.54 0 0 1-8.868-8.681l-.423-.706a3.128 3.128 0 0 1 .469-3.823l.844-.844a1.173 1.173 0 0 1 1.66 0z">
-                                </path>
-                            </svg>
-                            <span data-v-d64ec27c="">
-                                <span class="sr-only">Call Us</span>
-                            </span>
-                        </a>
-                    </li>
-                    <li><a href="https://www.facebook.com/" rel="noopener noreferrer" target="_blank"
-                            class="app-button pv-WjEgmP pc-YXWsTw !p-0 u-text-gray-900 focus-visible:ring-2"
-                            title="Follow Us on Facebook" data-v-d64ec27c="">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="fill-current text-[#1a77f2]" data-name="Layer 1"
-                                width="1em" height="1em" viewBox="0 0 24 24" id="facebook">
-                                <path
-                                    d="M15.12,5.32H17V2.14A26.11,26.11,0,0,0,14.26,2C11.54,2,9.68,3.66,9.68,6.7V9.32H6.61v3.56H9.68V22h3.68V12.88h3.06l.46-3.56H13.36V7.05C13.36,6,13.64,5.32,15.12,5.32Z">
-                                </path>
-                            </svg>
-                            <span data-v-d64ec27c=""><!--[--><span class="sr-only">Follow Us on
-                                    Facebook</span><!--]--></span><!---->
-                        </a></li>
-                </ul>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="tel:+84332006710" rel="noopener noreferrer" target="_blank"
+                                class="app-button pv-WjEgmP pc-lW04s6 !p-0 u-text-gray-900 focus-visible:ring-2"
+                                title="Call Us" data-v-d64ec27c="">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="fill-current text-[#4bd964] hover:scale-[1.2] transition-all duration-[0.3s] ease-in-out delay-[0ms]" id="phone"
+                                    width="1em" height="1em" version="1.1" viewBox="0 0 29 29" xml:space="preserve">
+                                    <path
+                                        d="M5.661 23.339c4.882 4.882 12.796 4.882 17.678 0 4.882-4.882 4.882-12.796 0-17.678s-12.796-4.882-17.678 0C.78 10.543.78 18.457 5.661 23.339zm5.545-16.613l1.86 1.86c.372.372.451.947.193 1.406l-.699 1.241a.781.781 0 0 0 .129.937l4.141 4.141a.781.781 0 0 0 .937.128l1.241-.699a1.173 1.173 0 0 1 1.406.193l1.861 1.861a1.175 1.175 0 0 1 0 1.66l-.881.881a3.135 3.135 0 0 1-3.786.493l-.084-.048a24.54 24.54 0 0 1-8.868-8.681l-.423-.706a3.128 3.128 0 0 1 .469-3.823l.844-.844a1.173 1.173 0 0 1 1.66 0z">
+                                    </path>
+                                </svg>
+                                <span data-v-d64ec27c="">
+                                    <span class="sr-only">Call Us</span>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/" rel="noopener noreferrer" target="_blank"
+                                class="app-button pv-WjEgmP pc-YXWsTw !p-0 u-text-gray-900 focus-visible:ring-2"
+                                title="Follow Us on Facebook" data-v-d64ec27c="">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="fill-current text-[#1a77f2] hover:scale-[1.2] transition-all duration-[0.3s] ease-in-out delay-[0ms]"
+                                    data-name="Layer 1" width="1em" height="1em" viewBox="0 0 24 24" id="facebook">
+                                    <path
+                                        d="M15.12,5.32H17V2.14A26.11,26.11,0,0,0,14.26,2C11.54,2,9.68,3.66,9.68,6.7V9.32H6.61v3.56H9.68V22h3.68V12.88h3.06l.46-3.56H13.36V7.05C13.36,6,13.64,5.32,15.12,5.32Z">
+                                    </path>
+                                </svg>
+                                <span data-v-d64ec27c=""><!--[--><span class="sr-only">Follow Us on
+                                        Facebook</span><!--]--></span><!---->
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
 
-        <!-- Dia chi va Dieu khoan... -->
-        <address class="mx-auto w-full max-w-container px-4 sm:px-6 lg:px-8">
-            <div
-                class="not-italic text-center flex items-center justify-center space-x-4 text-sm font-semibold leading-6 text-slate-700 ">
-                <a href="/terms-of-use ">Điều khoản sử dụng</a>
-                <div class="h-4 w-px bg-slate-500/20"></div><a href="/privacy-policy">Chính sách bảo mật</a>
-            </div>
-            <p class="mt-[16px] text-center text-sm leading-6 text-slate-500">
-                Địa chỉ: Toà nhà XXX, Ngõ 15 XXX, Quận 1, Thành Phố Hồ Chí Minh - Email: cskh@movingservice.com.vn - Điện
-                thoại liên hệ: 0332006710
-            </p>
-        </address>
+            <!-- Dia chi va Dieu khoan... -->
+            <address class="mx-auto w-full max-w-container px-4 sm:px-6 lg:px-8">
+                <div
+                    class="not-italic text-center flex items-center justify-center space-x-4 text-sm font-semibold leading-6 text-slate-700 ">
+                    <a href="/terms-of-use ">Điều khoản sử dụng</a>
+                    <div class="h-4 w-px bg-slate-500/20"></div><a href="/privacy-policy">Chính sách bảo mật</a>
+                </div>
+                <p class="mt-[16px] text-center text-sm leading-6 text-slate-500">
+                    Địa chỉ: Toà nhà XXX, Ngõ 15 XXX, Quận 1, Thành Phố Hồ Chí Minh - Email: cskh@movingservice.com.vn -
+                    Điện
+                    thoại liên hệ: 0332006710
+                </p>
+            </address>
+        </div>
     </footer>
 </template>
