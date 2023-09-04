b10e5ff99028955ce159bc8f575c31a827d5552a
KizunaYuuki <time0009000@gmail.com>
2023-09-04 16:14:46 +0700

    #0 Fix button

--- frontend/src/components/buttons/logout-button.vue
+++ frontend/src/components/buttons/logout-button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="button__logout" @click="handleLogout">Log Out</button>
+    <button class="hover:scale-[1.03] transition-all duration-[0.3s] ease-in-out delay-[0ms] my-[8px] inline-flex items-center px-[8px] py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-[#0096faee] hover:bg-[#0096fa]" @click="handleLogout">Log Out</button>
 </template>
   
 <script setup>
--- frontend/src/components/buttons/signup-button.vue
+++ frontend/src/components/buttons/signup-button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="button__sign-up" @click="handleSignUp">Sign Up</button>
+    <button class="hover:scale-[1.03] transition-all duration-[0.3s] ease-in-out delay-[0ms] my-[8px] inline-flex items-center px-[8px] py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-[#0096faee] hover:bg-[#0096fa]" @click="handleSignUp">Sign Up</button>
 </template>
   
 <script setup>
