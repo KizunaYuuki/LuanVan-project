23207351d81f27d6cde7d0b723f8c72154a78be7
KizunaYuuki <time0009000@gmail.com>
2023-09-09 14:20:51 +0700

    #0 Fix not found page

--- frontend/src/views/not-found-page.vue
+++ frontend/src/views/not-found-page.vue
@@ -1,6 +1,6 @@
 <template>
     <main>
-        <div class="flex h-[calc(100vh-208px)] items-center justify-center flex-col hover:scale-[1.01] transition-all duration-[0.3s] ease-in-out delay-[0ms]">
+        <div class="flex h-[calc(100vh-214px)] items-center justify-center flex-col hover:scale-[1.01] transition-all duration-[0.3s] ease-in-out delay-[0ms]">
             <h1 id="page-title" class="text-[60px]">Lost your way?</h1>
             <h2>Sorry, we can't find that page. You'll find lots to explore on the home page. </h2>
             <RouterLink to="/" class="my-[8px] inline-flex items-center px-[8px] py-2 
