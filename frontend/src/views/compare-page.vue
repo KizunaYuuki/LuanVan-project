b80f8486b4aff02b8b0da06fe2c157cd04b6aeae
KizunaYuuki <time0009000@gmail.com>
2023-09-10 20:12:27 +0700

    #0 Add compare-page

---
+++ frontend/src/views/compare-page.vue
@@ -0,0 +1,118 @@
+<template>
+    <main class="leading-[131%]">
+        <div class="bg-[#ffffffbe]">
+
+            <!-- PC -->
+            <div class="min-[1024px]:block hidden mx-auto max-w-[1280px] p-[32px]">
+                <div class="flex items-center border-b border-[#ecedef] min-h-[50px] mb-[32px] pb-[32px] w-full">
+                    <!-- <a href="/shopping/product/7709523306101892395?q=iphone+14&amp;sca_esv=564125615&amp;rlz=1C5CHFA_enVN1030VN1031&amp;biw=1378&amp;bih=758&amp;prds=eto:11631040611031032597_0,pid:3664884782502962629,rsk:PC_8053804293482199477" tabindex="-1" data-ved="0ahUKEwidr9Cx1p-BAxUaB4gKHUzuALQQyJ0ECA4"><img class="r4m4nf" src="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQV4X0Vmp5MVKv4eTBtp7lnzMCyIA0V2LsLTvhZb4Bla5X1avUvs_udIqxKOXl-44WFmG93OTBFZ33bv0pcRLAc3yfvqKS09AmTbx6IG5rskO35mp3gQIPI8Q&amp;usqp=CAY" data-atf="0" data-frt="0"></a> -->
+                    <!-- Tiêu đề -->
+                    <span class="leading-[28px] text-[22px]">So sánh Dịch vụ</span>
+                </div>
+
+                <!-- filters -->
+                <div class="inline-block">
+                    <div
+                        class="inline-flex items-center font-[500] bg-[#fff] hover:bg-[#d2e3fc36] border border-[#dadce0] rounded-[100px] h-[32px] mr-[12px] px-[8px] align-baseline">
+                        <button class="text-[#4d5156] text-[14px] leading-[32px] overflow-hidden px-[10px] text-ellipsis">
+                            <span>Lân
+                                cận</span>
+                        </button>
+                    </div>
+                    <div
+                        class="inline-flex items-center font-[500] bg-[#fff] hover:bg-[#d2e3fc36] border border-[#dadce0] rounded-[100px] h-[32px] mr-[12px] px-[8px] align-baseline">
+                        <button class="text-[#4d5156] text-[14px] leading-[32px] overflow-hidden px-[10px] text-ellipsis">
+                            <span>Giao
+                                hàng miễn phí</span>
+                        </button>
+                    </div>
+                    <div
+                        class="inline-flex items-center font-[500] bg-[#fff] hover:bg-[#d2e3fc36] border border-[#dadce0] rounded-[100px] h-[32px] mr-[12px] px-[8px] align-baseline">
+                        <button
+                            class="text-[#4d5156] text-[14px] leading-[32px] overflow-hidden px-[10px] text-ellipsis"><span>Mới</span>
+                        </button>
+                    </div>
+                </div>
+
+                <!-- content -->
+                <div class="mt-[1rem] flow-root">
+                    <div class="overflow-x-auto">
+                        <div class="align-middle min-w-[100%] inline-block">
+                            <table
+                                class="min-w-[100%] indent-0 border rounded-[8px] border-separate border-spacing-0 table-fixed">
+                                <thead>
+                                    <tr class="">
+                                        <th scope="col"
+                                            class="pl-[1rem] min-[640px]:pl-0 text-[#70757a] font-[600] text-[12px] leading-[1.25rem] text-left pr-[.75rem] py-[.875rem]">
+                                            Nhà cung cấp dịch vụ</th>
+                                        <th scope="col"
+                                            class="min-[640px]:pl-0 text-[#70757a] font-[600] text-[12px] leading-[1.25rem] text-left px-[.75rem] py-[.875rem]">
+                                            Thời gian vận chuyển</th>
+                                        <th scope="col"
+                                            class="min-[640px]:pl-0 text-[#111827] font-[600] text-[12px] leading-[1.25rem] text-left px-[.75rem] py-[.875rem]">
+                                            Giá dịch vụ</th>
+                                        <th scope="col"
+                                            class="min-[640px]:pl-0 text-[#70757a] font-[600] text-[12px] leading-[1.25rem] text-left pr-[1rem] py-[.875rem] relative">
+                                            <span class="sr-only">Xoá</span>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody class="">
+                                    <tr class="mx-[4px]">
+                                        <td
+                                            class="border-[#dadce0] border-t pl-[1rem] min-[640px]:pl-0 text-[#111827] font-[500] text-[.875rem] leading-[1.25rem] pr-[.75rem] py-[1rem] whitespace-nowrap">
+                                            FedEx</td>
+                                        <td
+                                            class="border-[#dadce0] border-t text-[#6b7280] font-[500] text-[.875rem] leading-[1.25rem] px-[.75rem] py-[1rem] whitespace-nowrap">
+                                            1h</td>
+                                        <td
+                                            class="border-[#dadce0] border-t text-[#6b7280] font-[500] text-[.875rem] leading-[1.25rem] px-[.75rem] py-[1rem] whitespace-nowrap">
+                                            860000Đ</td>
+                                        <td
+                                            class="border-[#dadce0] border-t min-[640px]:pr-0 font-[500] text-[.875rem] leading-[1.25rem] text-right pr-[1rem] py-[1rem] whitespace-nowrap relative">
+                                            <div class="float-right">
+                                                <a href="#" class="text-[#0096fa] inherit py-[17px]" target="_blank">
+                                                    <div
+                                                        class="bg-[#e8f0fe] box-border rounded-[8px] text-[#1a73e8] inline-block text-[12px] leading-[20px] py-[6px] px-[10px] text-center w-[124px]">
+                                                        Truy cập</div>
+                                                    <span class="sr-only">Truy cập</span>
+                                                </a>
+                                            </div>
+
+                                        </td>
+                                    </tr>
+                                    <tr class="mx-[4px]">
+                                        <td
+                                            class="border-[#dadce0] border-t pl-[1rem] min-[640px]:pl-0 text-[#111827] font-[500] text-[.875rem] leading-[1.25rem] pr-[.75rem] py-[1rem] whitespace-nowrap">
+                                            FedEx</td>
+                                        <td
+                                            class="border-[#dadce0] border-t text-[#6b7280] font-[500] text-[.875rem] leading-[1.25rem] px-[.75rem] py-[1rem] whitespace-nowrap">
+                                            1h</td>
+                                        <td
+                                            class="border-[#dadce0] border-t text-[#6b7280] font-[500] text-[.875rem] leading-[1.25rem] px-[.75rem] py-[1rem] whitespace-nowrap">
+                                            860000Đ</td>
+                                        <td
+                                            class="border-[#dadce0] border-t min-[640px]:pr-0 font-[500] text-[.875rem] leading-[1.25rem] text-right pr-[1rem] py-[1rem] whitespace-nowrap relative">
+                                            <div class="float-right">
+                                                <a href="#" class="text-[#0096fa] inherit py-[17px]" target="_blank">
+                                                    <div
+                                                        class="bg-[#e8f0fe] box-border rounded-[8px] text-[#1a73e8] inline-block text-[12px] leading-[20px] py-[6px] px-[10px] text-center w-[124px]">
+                                                        Truy cập</div>
+                                                    <span class="sr-only">Truy cập</span>
+                                                </a>
+                                            </div>
+
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Moblie -->
+            <div></div>
+        </div>
+    </main>
+</template>
