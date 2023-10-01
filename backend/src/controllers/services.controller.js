c791cdea3225859625b874ba583b7a5dbbd8c0e0
KizunaYuuki <time0009000@gmail.com>
2023-10-01 10:11:32 +0700

    #0 Fix BE

--- backend/src/controllers/services.controller.js
+++ backend/src/controllers/services.controller.js
@@ -3,12 +3,19 @@ const { pool } = require("../models/db.js");
 // Lay tat ca services
 async function getServices() {
     // const [rows] = await pool.query("select * from services")
-    const [rows] = await pool.query(`SELECT services.id as service_id, services.name as service_name, services.description, services.delivery_date, services.weight, services.price,
-    providers.id, providers.name as provider_name, providers.image
+    const [rows] = await pool.query(`SELECT AVG(reviews.rate) AS average_rate, COUNT(reviews.id) as totalCount, total.service_id, total.service_name, total.description,
+    total.delivery_date, total.weight, total.price, total.provider_id, total.provider_name, total.image
+    FROM
+    (SELECT services.id as service_id, services.name as service_name, services.description, 
+    services.delivery_date, services.weight, services.price,
+    providers.id as provider_id, providers.name as provider_name, providers.image
     FROM services
     JOIN service_types ON services.service_type_id = service_types.id
     JOIN providers ON service_types.providers_id = providers.id
-    ORDER BY services.id`)
+    ORDER BY services.id) AS total
+    LEFT JOIN reviews ON reviews.service_id = total.service_id
+    GROUP BY total.service_id, total.service_name
+    ORDER BY total.service_id`)
     return rows
 }
 
