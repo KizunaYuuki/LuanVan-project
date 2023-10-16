46e04d5dd3dbf00e87fbf9677fb117257ac5f4b1
KizunaYuuki <time0009000@gmail.com>
2023-10-16 08:42:01 +0700

    #0 Fix FE

--- frontend/src/views/callback-page.vue
+++ frontend/src/views/callback-page.vue
@@ -1,12 +1,6 @@
 <template>
-    <main>
-        <div
-            class="flex h-[calc(100vh)] items-center justify-center flex-col hover:scale-[1.01] transition-all duration-[0.3s] ease-in-out delay-[0ms]">
-            <h1 id="page-title" class="text-[60px]">Something are wrong</h1>
-            <RouterLink to="/" class="my-[8px] inline-flex items-center px-[8px] py-2 
-                font-semibold leading-6 text-sm shadow rounded-md text-white bg-[#0096faee] hover:bg-[#0096fa]">Về
-                Trang chủ</RouterLink>
-            <h3>Error Code <strong>Login CallBack</strong></h3>
-        </div>
-    </main>
+    <PageLoader />
 </template>
+<script setup>
+import PageLoader from "@/components/page-loader.vue";
+</script>
