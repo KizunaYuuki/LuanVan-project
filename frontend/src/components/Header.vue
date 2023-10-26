c167d478e59922853a45b53392d2d5dc7ecdcf9b
KizunaYuuki <time0009000@gmail.com>
2023-10-26 09:44:58 +0700

    #0 Fix FE

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -51,9 +51,9 @@
                 class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                 <div class="flex items-center justify-between">
                     <RouterLink to="/"
-                        class="p-1.5 text-[1rem] text-[#0097f9] subpixel-antialiased hover:underline hover:underline-offset-[2px]">
-                        <span class="sr-only">Freight service</span>
-                        Freight service
+                        class="text-lg font-semibold text-[#0097f9] subpixel-antialiased hover:underline hover:underline-offset-[2px]">
+                        <span class="sr-only">BestChoice</span>
+                        BestChoice
                     </RouterLink>
                     <button type="button" class="rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
                         <span class="sr-only">Close menu</span>
